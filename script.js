865d48850be6949d02a4ac6885a0f74bf51c91f2
Gautam <gautamnimase007@gmail.com>
2025-08-18 13:54:06 +0530

    obj

---
+++ script.js
@@ -0,0 +1,15 @@
+const mySym=Symbol("key1")
+
+const myObj={
+    name:"Gautam",
+    [mySym]:"key21",
+    age:21,
+    location:"pune",
+    "full name":"Gautam Ashok Nimase"
+}
+
+console.log(myObj.name)
+console.log(myObj["name"])
+console.log(myObj[mySym]);
+
+console.log(myObj)
