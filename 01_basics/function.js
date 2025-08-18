84d5d5ad2eb041646ba731c4f0b75a0cbf57826b
Gautam <gautamnimase007@gmail.com>
2025-08-18 18:49:00 +0530

    function

---
+++ 01_basics/function.js
@@ -0,0 +1,43 @@
+/*
+
+function add(num1,num2){
+    console.log(num1+num2)
+}
+let result=add(4,5)
+console.log(result);
+
+*/
+
+/*
+
+when we want to return value
+function add(num1,num2){
+    let result=num1+num2
+    return result
+}
+
+console.log(add(4,6));
+
+const result=add(7,8)
+console.log(result)
+*/
+
+/*
+when we want to return value
+function add(num1, num2){
+    return num1 +num2
+}
+
+const result=add(4,5)
+console.log(result);
+*/
+
+/*
+string type of function
+
+function isLoggedIn(name="gautam"){
+    return `hello ${name}  just logged in `
+}
+
+console.log(isLoggedIn("sanu"));
+*/
