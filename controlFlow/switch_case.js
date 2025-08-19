780d2838c93342df9b111fd43098dfd826549ad1
Gautam <gautamnimase007@gmail.com>
2025-08-19 23:08:31 +0530

    switch case

---
+++ controlFlow/switch_case.js
@@ -0,0 +1,58 @@
+/*
+syntax of swich case
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+/*
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+*/
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
