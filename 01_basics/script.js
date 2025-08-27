58890f651723a22e08bd0c86dae10ab26229d662
Gautam <gautamnimase007@gmail.com>
2025-08-27 23:22:16 +0530

    java script

--- 01_basics/script.js
+++ 01_basics/script.js
@@ -1,16 +1,23 @@
 const mySym=Symbol("key1")
 
-const myObj={
-    name:"Gautam",
-    [mySym]:"key21",
-    age:21,
-    location:"pune",
-    "full name":"Gautam Ashok Nimase"
-}
+// const myObj={
+//     name:"Gautam",
+//     [mySym]:"key21",
+//     age:21,
+//     location:"pune",
+//     "full name":"Gautam Ashok Nimase"
+// }
 
-// console.log(myObj.name)
-// console.log(myObj["name"])
-// console.log(myObj[mySym]);
+// // console.log(myObj.name)
+// // console.log(myObj["name"])
+// // console.log(myObj[mySym]);
 
-// console.log(myObj)
-console.log("hello word")
+// // console.log(myObj)
+// console.log("hello word")
+
+
+console.log("hello word");
+let a=10
+let b=20;
+let c=a+b;
+console.log(c);
